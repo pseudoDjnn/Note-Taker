01427f5d7cf685f834d330e55008b2dd2320a9cd
p5eudoDjnn <alexander.havers@gmail.com>
2022-09-24 16:11:11 -0400

    added router export and public html file to load on localhost launch

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+// ROUTES FOR SERVER
+const apiRoutes = require("./routes/apiRoutes");
+
 // PORT SPCIFICITY
 const PORT = process.env.PORT || 3001;
 
 // INSTANCE OF SERVER
 const app = express();
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+
+// DB JSON DATA
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`listen on http://localhost:${PORT}`);
