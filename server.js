9c3c526df211116011ab34120595b16e224e867e
p5eudoDjnn <alexander.havers@gmail.com>
2022-09-24 12:54:44 -0400

    added localhost to throw get error from terminal

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+// DEPENDENCIES
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
+// PORT SPCIFICITY
+const PORT = process.env.PORT || 3001;
+
+// INSTANCE OF SERVER
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`listen on http://localhost:${PORT}`);
+});
